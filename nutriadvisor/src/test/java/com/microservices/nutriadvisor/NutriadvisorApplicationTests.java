24539366932ee90809e43db59374298327eddbf7
laviniastn <laviniastn25@gmail.com>
2022-05-14 14:30:34 +0300

    microservices

---
+++ nutriadvisor/src/test/java/com/microservices/nutriadvisor/NutriadvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.nutriadvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriadvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
